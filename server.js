32074cfb56faf82a64daaaf79f6379cb46408b7d
nonha <manish06598@gmail.com>
2026-07-12 14:11:36 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import express from 'express';
+require("dotenv").config();
+import express from "express";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
 const PORT = process.env.PORT || 3000;
