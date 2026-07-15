3364b42a514a552699d7d7c1bf3f929defcfd1f9
nonha <manish06598@gmail.com>
2026-07-16 00:12:14 +0530

    mongodb conncetion problem resolve

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.URL);
+    await mongoose.connect(process.env.MONGODB_URI);
 
     console.log("✅ Connected to MongoDB");
   } catch (error) {
